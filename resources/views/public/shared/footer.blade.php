cf631487500881f407824064ed141e15f75da20e
kietnguyen123zzzz <82712093+kietnguyen123zzzz@users.noreply.github.com>
2021-04-17 21:05:19 +0700

    update

--- resources/views/public/shared/footer.blade.php
+++ resources/views/public/shared/footer.blade.php
@@ -2,10 +2,10 @@
         <div class="max-w-screen-lg mx-auto py-4">
             <div class="flex justify-between">
                 <div>
-                    <p><i class="fas fa-map-marker-alt"></i>  Nguyễn Việt Hồng - Ninh Kiều - Cần Thơ </p>
-                    <p><i class="fas fa-phone"></i>  0888964449 </p>
-                    <a href="mailto:kiet051000@gmail.com"><i class="fas fa-envelope"></i> kiet051000@gmail.com</a>
-                    <P> <i class="fab fa-facebook"></i> Kiệt Nguyễn </P>
+                    <p><i class="fas fa-map-marker-alt"></i> Nguyễn Việt Hồng - Ninh Kiều - Cần Thơ </p>
+                    <p><a href="tel:0888964449"><i class="fas fa-phone"></i>  0888964449 </a></p>
+                    <p><a href="mailto:kiet051000@gmail.com"><i class="fas fa-envelope"></i> kiet051000@gmail.com</a> </p>
+                    <P><a href="https://www.facebook.com/profile.php?id=100006360066252"><i class="fab fa-facebook"></i> Kiệt Nguyễn </a></P>
                 </div>
                 <div>
                     <p>ĐỒNG HỒ QUỐC KIỆT</p>
